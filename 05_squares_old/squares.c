de0598f522504d12319d64ec0528a616af4aed3f
Coursera Learner <nobody@nowhere.nul>
2020-12-12 08:55:24 +0000

    Resetting an assignment

--- 05_squares_old/squares.c
+++ 05_squares_old/squares.c
@@ -27,25 +27,26 @@ int isAtBorder(int coord, int offset, int size) {
   else
     return 0;
 }
+int max (int a, int b){
+  if (a < b){
+    return b;}
+  else{
+    return a;}
+}
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-  int w = 0;
-  if (size1 < (x_offset +size2))
-    w = x_offset+size2;
-  else
-    w = size1;
   //compute the max of size1 and (y_offset + size2).  Call this h
-  int h = 0 ;
-  if (size1 < (y_offset+size2))
-    h = y_offset+size2;
-  else
-    h = size1;
+  int w = max(size1,x_offset+size2);
+  int h = max (size1, y_offset+size2);
+  printf("w %d ---- -h %d", w,h);
 
   //count from 0 to h. Call the number you count with y
   for (int y = 0 ; y < h; y++){
+    printf("%d",y);
     //count from 0 to w. Call the number you count with x
-    for ( int x = 0 ; x < w ; x++){
+    for (int x = 0 ; x < w ; x++){
+      printf("%d",x);
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
@@ -53,12 +54,13 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
-      if (((isInRange(x,x_offset,size2) == 1)&&
+      
+       if (((isInRange(x,x_offset,size2) == 1)&&
 	   (isAtBorder(y,y_offset,y_offset+size2-1) == 1))||
 	  ((isInRange(y,y_offset,size2) ==1)&&
-	   (isAtBorder(x,x_offset,x_offset+size2-1)==1)))
+	   (isAtBorder(x,x_offset,x_offset+size2-1)==1))){
 	printf("*");
-
+       }
       //if not,
       // check if EITHER
       //    x is less than size1 AND (y is either 0 or size1-1)
@@ -66,16 +68,16 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
       else if(((x < size1) && ((y = 0) || (y = size1-1)))||
-	      ((y < size1) && ((x = 0) || (x = size1-1))))
+	      ((y < size1) && ((x = 0) || (x = size1-1)))){
 	printf("#");
-
+      }
       //else print a space
-      else
-	printf(" ");
+      else{
+      printf(" ");
+      }
     //when you finish counting x from 0 to w, 
     //print a newline
     }
-    printf("\n");
+      printf("\n");
   }
-
 }
