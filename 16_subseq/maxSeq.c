54c096dfa99c275e1a496c7c2586bd2d2a3d11ef
Coursera Learner <nobody@nowhere.nul>
2021-02-24 15:24:30 +0000

    16

--- 16_subseq/maxSeq.c
+++ 16_subseq/maxSeq.c
@@ -1,17 +1,21 @@
-#include<stdio.h>
-#include<stdlib.h>
-size_t maxSeq(int * array, size_t n){
-  size_t  lenth= 1;
-  size_t maxlen=0;
-  if (n == 0 ) return 0;
+#include <stdio.h>
+#include <stdlib.h>
 
-  for(size_t i=1 ; i<n ;i++ ){
-    if (array[i] > array[i-1]){lenth += 1;}
-    else {
-      if (lenth > maxlen) maxlen = lenth;
-      lenth=1;
+
+size_t maxSeq(int * array, size_t n){
+  size_t maxseq = 0;
+  size_t tempmax = 1 ;
+  size_t current = array[0];
+  if ( n == 0){return 0;}
+
+  for (size_t i = 1; i < n; i++){
+    if (current < array[i]){tempmax ++;}
+    else{
+      if (tempmax > maxseq){maxseq = tempmax;}
+      tempmax = 1;
     }
+    current = array[i];
   }
-  if (lenth > maxlen) maxlen = lenth;
-  return maxlen;
-}
+  if (tempmax > maxseq){maxseq = tempmax;}
+  return maxseq;
+} 
